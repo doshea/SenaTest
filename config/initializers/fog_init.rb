4009cf88455714befd399d4566b83797042b90ba
Dylan O'Shea <dylan.j.oshea@gmail.com>
2013-06-19 21:23:01 -0400

    Moving fog config temp directory line into the class (it was a type)

--- config/initializers/fog_init.rb
+++ config/initializers/fog_init.rb
@@ -9,9 +9,11 @@ CarrierWave.configure do |config|
   config.fog_public     = false                                     # optional, defaults to true
   # config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
   # config.asset_host   = 'https://assets.example.com'              # optional, defaults to nil
-end
 
-config.cache_dir = "#{Rails.root}/tmp/uploads"                     # Generate http:// urls. Defaults to :authenticated_read (https://)
+  config.cache_dir = "#{Rails.root}/tmp/uploads"                     # Generate http:// urls. Defaults to :authenticated_read (https://)
+
+
+end
 
 CarrierWave.configure {|config| config.fog_directory = 'senatest-development'} if Rails.env.development?
 CarrierWave.configure {|config| config.fog_directory = 'senatest-test'} if Rails.env.test?
